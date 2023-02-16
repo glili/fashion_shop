befcdff15b7abcd8926d5a89a9957ae2e6e970c1
Lili <you@example.com>
2023-02-15 23:54:25 -0500

    home, presentation, product pages

---
+++ script/script_projet.js
@@ -0,0 +1,10 @@
+// //Slick slider initialize
+// $('.slider').slick({
+//     arrows:false, dots: false, infinite:true, speed:500,
+//     autoplay:true, autoplaySpeed: 3000, slidesToShow:1, slidesToScroll:1
+//   });
+//   //On click of slider-nav childern,
+//   //Slick slider navigate to the respective index.
+//   $('.slider-nav > div').click(function() {
+//       $('.slider').slick('slickGoTo',$(this).index());
+//   })
