94d24999bf26c6b0b5a775b154d8e7c8f0475a66
Lili <you@example.com>
2023-02-19 22:41:45 -0500

    js finished

--- script/script_projet.js
+++ script/script_projet.js
@@ -29,143 +29,148 @@ function slideLeft() {
   current--;
 }
 
-// Bouton précédent
-document.querySelector("#previous").addEventListener("click", function () {
-  if (current === 0) {
-    current = sliderImages.length;
-  }
-  slideLeft();
-});
+// // Bouton précédent
+// document.querySelector("#previous").addEventListener("click", function () {
+//   if (current === 0) {
+//     current = sliderImages.length;
+//   }
+//   slideLeft();
+// });
 
-// Bouton suivant
-document.querySelector("#next").addEventListener("click", function () {
-  if (current === sliderImages.length - 1) {
-    current = -1;
-  }
-  slideRight();
-});
+// // Bouton suivant
+// document.querySelector("#next").addEventListener("click", function () {
+//   if (current === sliderImages.length - 1) {
+//     current = -1;
+//   }
+//   slideRight();
+// });
 
-// Défilement automatique toutes les 2 secondes
-setInterval(function () {
-  if (current === sliderImages.length - 1) {
-    current = -1;
-  }
-  slideRight();
-}, 2000);
+// // Défilement automatique toutes les 2 secondes
+// setInterval(function () {
+//   if (current === sliderImages.length - 1) {
+//     current = -1;
+//   }
+//   slideRight();
+// }, 2000);
 
-startSlide();
+// startSlide();
 
 
 
 
 // Validation
-
-function verificationInfo() {
+function validationInfo(name) {
   //var de validation
-  var fieldVerif = true;
-
-  var preNom = document.getElementById("fname").value;
-  var nom = document.getElementById("lname").value;
-  var password = document.getElementById("password").value;
-  var cpassword = document.getElementById("cpassword").value;
-  var email = document.getElementById("email").value;
-  var email1 = document.getElementById("email1").value;
-  var email2 = document.getElementById("email2").value;
-  var email3 = document.getElementById("email3").value;
-
-
-//   if (preNom === "" || preNom === undefined) {
-//     fieldVerif = false;
-//     alert("Le champs prenom ne peut pas etre vide");
-//   }
-//   if (nom === "" || nom === undefined) {
-//     fieldVerif = false;
-//     alert("Le champs nom ne peut pas etre vide");
-//   }
-//   if (password === "" || password === undefined) {
-//     fieldVerif = false;
-//     alert("Le champs mot de passe ne peut pas etre vide");
-//   }
-//   if (cpassword === "" || cpassword === undefined) {
-//     fieldVerif = false;
-//     alert("Le champs confirmation de mot de passe ne peut pas etre vide");
-//   }
-
-//   if (password != cpassword) {
-//     alert("Le deux mots de passe sont differents");
-// }
-
-
-  var btn1 = document.getElementById("submit1");
-  var btn2 = document.getElementById("submit2");
-  var btn3 = document.getElementById("submit3");
-  var btn4 = document.getElementById("info-register");
-
-  if (btn1.buttonClicked) {
-    if (preNom === "" || preNom === undefined) {
-      fieldVerif = false;
+ var fieldVerif = true;
+  if (name == "form-inscription") {
+    var preNom = document.forms["form-inscription"]["fname"].value;
+    if ( preNom === undefined || preNom === "") {
       alert("Le champs prenom ne peut pas etre vide");
+      return false;
     }
+
+    var nom = document.forms["form-inscription"]["lname"].value;
     if (nom === "" || nom === undefined) {
-      fieldVerif = false;
       alert("Le champs nom ne peut pas etre vide");
+      return false;
     }
+
+    var password = document.forms["form-inscription"]["password"].value;
     if (password === "" || password === undefined) {
-      fieldVerif = false;
       alert("Le champs mot de passe ne peut pas etre vide");
+      return false;
     }
+
+    var cpassword = document.forms["form-inscription"]["cpassword"].value;
     if (cpassword === "" || cpassword === undefined) {
-      fieldVerif = false;
       alert("Le champs confirmation de mot de passe ne peut pas etre vide");
-    }
-  
-    if (email === "") {
-      fieldVerif = false;
-      alert("Le champs couriel ne peut pas etre  vide");
-    }
-  } else if (btn2.buttonClicked) {
-    if (email2 === "") {
-      fieldVerif = false;
-      alert("Le champs couriel ne peut pas etre  vide");
-    }
-  } else if (btn3.buttonClicked) {
-    if (email3 === "") {
-      fieldVerif = false;
-      alert("Le champs couriel ne peut pas etre  vide");
-    }
-  } else if (btn4.buttonClicked) {
-    if (email1 === "") {
-      fieldVerif = false;
-      alert("Le champs couriel ne peut pas etre  vide");
+      return false;
     }
 
-  } 
-    var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/;
-    var regexTest = verif.test(String(contenuEmail).toLocaleLowerCase());
-    if (!regexTest) {
-      fieldVerif = false;
-      alert("Verifiez la syntaxe de courriel");
+    if (password != cpassword) {
+      alert("Le deux mots de passe sont differents");
+      return false;
     }
-  
- }
-
-
     
+    var email = document.forms["form-inscription"]["email"].value;
+    if (email === "") {
+      alert("Le champs couriel ne peut pas etre  vide");
+      return false;
+    } else {
+      var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/;
+      var regexTest = emailRegex.test(String(email).toLocaleLowerCase());
+      if (!regexTest) {
+        alert("Verifiez la syntaxe de courriel");
+        return false;
+      }
+    }
+
+  } else if (name == "form-compte") {
+    var email = document.forms["form-compte"]["email"].value;
+    if (email === "") {
+      alert("Le champs couriel ne peut pas etre  vide");
+      // fieldVerif = false;
+      return false;
+    } else {
+      var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/;
+      var regexTest = emailRegex.test(String(email).toLocaleLowerCase());
+      if (!regexTest) {
+        alert("Verifiez la syntaxe de courriel");
+        return false;
+      }
+    }
+    var password = document.forms["form-compte"]["password"].value;
+    if (password === "" || password === undefined) {
+      alert("Le champs mot de passe ne peut pas etre vide");
+      return false;
+    }
+  } else if (name == "form-contact") {
+    var email = document.forms["form-contact"]["email"].value;
+    if (email === "") {
+      alert("Le champs couriel ne peut pas etre  vide");
+      return false;
+    } else {
+      var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/;
+      var regexTest = emailRegex.test(String(email).toLocaleLowerCase());
+      if (!regexTest) {
+        alert("Verifiez la syntaxe de courriel");
+        return false;
+      }
+    }
+  } else if (name == "form-footer") {
+    var email = document.forms["form-footer"]["email"].value;
+    if (email === "") {
+      alert("Le champs couriel ne peut pas etre  vide");
+      return false;
+    } else {
+      var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/;
+      var regexTest = emailRegex.test(String(email).toLocaleLowerCase());
+      if (!regexTest) {
+        alert("Verifiez la syntaxe de courriel");
+        return false;
+      }
+    }
+  }
+  
+ 
+}
 
 
 
 
 
 
-var btns = document.getElementsByTagName("button");
-var navs = document.getElementsByTagName("nav");
-var foots = document.getElementsByTagName("footer");
-for (let i = 0; i < btns.length; i++) { 
-  btns[i].addEventListener("mouseover", ()=>btns[i].style.backgroundColor="green");
-  foots[i].addEventListener("mouseover", ()=>foots[i].style.backgroundColor="gray");
-  navs[i].addEventListener("mouseover", ()=>navs[i].style.backgroundColor="gray");
-};
 
-function pageLoaded() { alert("La page est chargée !"); }
-function imageHovered() { alert("L'image est survolée !"); }
-function buttonClicked() { alert("Le bouton est cliqué !"); }
+
+// var btns = document.getElementsByTagName("button");
+// var navs = document.getElementsByTagName("nav");
+// var foots = document.getElementsByTagName("footer");
+// for (let i = 0; i < btns.length; i++) { 
+//   btns[i].addEventListener("mouseover", ()=>btns[i].style.backgroundColor="green");
+//   foots[i].addEventListener("mouseover", ()=>foots[i].style.backgroundColor="gray");
+//   navs[i].addEventListener("mouseover", ()=>navs[i].style.backgroundColor="gray");
+// };
+
+// function pageLoaded() { alert("La page est chargée !"); }
+// function imageHovered() { alert("L'image est survolée !"); }
+// function buttonClicked() { alert("Le bouton est cliqué !"); }
